1ddd3f3e9fd334246ea88662114c62e487a8285a
Kent Miguel <kentmiguel10@gmail.com>
2022-09-12 12:34:11 +0200

    function to swap token for eth and vice versa

--- packages/hardhat/contracts/DEX.sol
+++ packages/hardhat/contracts/DEX.sol
@@ -26,12 +26,22 @@ contract DEX {
     /**
      * @notice Emitted when ethToToken() swap transacted
      */
-    event EthToTokenSwap();
+    event EthToTokenSwap(
+        address indexed buyer,
+        string message,
+        uint256 value,
+        uint256 tokenOutput
+    );
 
     /**
      * @notice Emitted when tokenToEth() swap transacted
      */
-    event TokenToEthSwap();
+    event TokenToEthSwap(
+        address indexed buyer,
+        string message,
+        uint256 ethOutput,
+        uint256 tokenOutput
+    );
 
     /**
      * @notice Emitted when liquidity provided to DEX and mints LPTs.
@@ -57,7 +67,16 @@ contract DEX {
      * @return totalLiquidity is the number of LPTs minting as a result of deposits made to DEX contract
      * NOTE: since ratio is 1:1, this is fine to initialize the totalLiquidity (wrt to balloons) as equal to eth balance of contract.
      */
-    function init(uint256 tokens) public payable returns (uint256) {}
+    function init(uint256 tokens) public payable returns (uint256) {
+        require(totalLiquidity == 0, "DEX: init - already has liquidity");
+        totalLiquidity = address(this).balance;
+        liquidity[msg.sender] = totalLiquidity;
+        require(
+            token.transferFrom(msg.sender, address(this), tokens),
+            "DEX: init - transfer did not transact"
+        );
+        return totalLiquidity;
+    }
 
     /**
      * @notice returns yOutput, or yDelta for xInput (or xDelta)
@@ -67,7 +86,13 @@ contract DEX {
         uint256 xInput,
         uint256 xReserves,
         uint256 yReserves
-    ) public view returns (uint256 yOutput) {}
+    ) public pure returns (uint256 yOutput) {
+        uint256 xInputWithFee = xInput.mul(997);
+        uint256 numerator = xInputWithFee.mul(yReserves);
+        uint256 denominator = (xReserves.mul(1000)).add(xInputWithFee);
+        yOutput = (numerator / denominator);
+        return yOutput;
+    }
 
     /**
      * @notice returns liquidity for a user. Note this is not needed typically due to the `liquidity()` mapping variable being public and having a getter as a result. This is left though as it is used within the front end code (App.jsx).
@@ -79,15 +104,46 @@ contract DEX {
     /**
      * @notice sends Ether to DEX in exchange for $BAL
      */
-    function ethToToken() public payable returns (uint256 tokenOutput) {}
+    function ethToToken() public payable returns (uint256 tokenOutput) {
+        require(msg.value > 0, "cannot swap 0 ETH");
+        uint256 ethReserve = address(this).balance.sub(msg.value);
+        uint256 token_reserve = token.balanceOf(address(this));
+        tokenOutput = price(msg.value, ethReserve, token_reserve);
+
+        require(
+            token.transfer(msg.sender, tokenOutput),
+            "ethToToken(): reverted swap."
+        );
+        emit EthToTokenSwap(
+            msg.sender,
+            "Eth to Balloons",
+            msg.value,
+            tokenOutput
+        );
+        return tokenOutput;
+    }
 
     /**
      * @notice sends $BAL tokens to DEX in exchange for Ether
      */
-    function tokenToEth(uint256 tokenInput)
-        public
-        returns (uint256 ethOutput)
-    {}
+    function tokenToEth(uint256 tokenInput) public returns (uint256 ethOutput) {
+        require(tokenInput > 0, "cannot swap 0 tokens");
+        uint256 token_reserve = token.balanceOf(address(this));
+        ethOutput = price(tokenInput, token_reserve, address(this).balance);
+        require(
+            token.transferFrom(msg.sender, address(this), tokenInput),
+            "tokenToEth(): reverted swap."
+        );
+        (bool sent, ) = msg.sender.call{value: ethOutput}(""); //transfer eth to user from dex
+        require(sent, "tokenToEth: revert in transferring eth to you!");
+        emit TokenToEthSwap(
+            msg.sender,
+            "Balloons to ETH",
+            ethOutput,
+            tokenInput
+        );
+        return ethOutput;
+    }
 
     /**
      * @notice allows deposits of $BAL and $ETH to liquidity pool
